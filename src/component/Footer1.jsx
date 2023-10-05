d427547b7dd8efbe3a5212d6f115e46b3589071d
batoulll-ghosn <batoul.ghosn@isae.edu.lb>
2023-10-05 11:11:06 +0300

    last version with front connected

--- src/component/Footer1.jsx
+++ src/component/Footer1.jsx
@@ -1,66 +1,72 @@
-import React from 'react'
-import "./Footer1.css"
-import PhoneCall from "../icons/PhoneCall.svg";
-import Github from "../icons/Github.svg";
-import Instagram from "../icons/Instagram.svg";
-import Facebook from "../icons/Facebook.jpg";
-
-
+import React, { useEffect, useState } from 'react';
+import './Footer1.css';
+import PhoneCall from '../icons/PhoneCall.svg';
+import Github from '../icons/Github.svg';
+import Instagram from '../icons/Instagram.svg';
+import Facebook from '../icons/Facebook.jpg';
 
 const Footer1 = () => {
+    const [footerData, setFooterData] = useState(null);
+
+    useEffect(() => {
+        // Fetch data from the API
+        fetch('http://localhost:5000/footer/getfooter')
+            .then((response) => response.json())
+            .then((data) => {
+                // Set the fetched data to the state
+                setFooterData(data.data[0]);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
     return (
-        <div className='foots'>
-            <div className='Flex_containerz'>
-                <div className='flex-itemz'>
-                    <p> Our Addreses</p>
-                    <p style={{ color: 'white', }}>Beirut, Lebanon</p>
-                    <p> Our HeadQuarters</p>
-                    <p style={{ color: 'white', }}> Dubai, U.A.E.</p>
+        <div className="foots">
+            <div className="Flex_containerz">
+                <div className="flex-itemz">
+                    <p> Our Addresses</p>
+                    {footerData && (
+                        <>
+                            <p style={{ color: 'white' }}>{footerData.address}, Lebanon</p>
+                            <p> Our Headquarters</p>
+                            <p style={{ color: 'white' }}>{footerData.quater}, U.A.E.</p>
+                        </>
+                    )}
                 </div>
-                {/* const  */}
-                <div className='flex-itemz'>
-                    <p class="social-media"> Our Phones</p>
-                    <p style={{ color: 'white', }}>Office  Phone:  +96104606987</p>
-                    <p style={{ color: "white" }}>Mobile Phone:   +96170606987</p>
+                <div className="flex-itemz">
+                    <p className="social-media"> Our Phones</p>
+                    {footerData && (
+                        <>
+                            <p style={{ color: 'white' }}>Office Phone: {footerData.ophone}</p>
+                            <p style={{ color: 'white' }}>Mobile Phone: {footerData.mobilephone}</p>
+                        </>
+                    )}
                 </div>
 
-                <div className='flex-itemz'>
-
+                <div className="flex-itemz">
                     <p>Follow us on social media:</p>
-                    <div class="social-media">
-
-                        <a href="https://www.facebook.com" target='_blank' rel="noreferrer" >
-                            <img src={Facebook} style={{ height: "25px", fill: 'white' }} alt='Facebook' />
+                    <div className="social-media">
+                        <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
+                            <img src={Facebook} style={{ height: '25px', fill: 'white' }} alt="Facebook" />
                         </a>
-                        {/* </div>
-                    <div class="social-media"> */}
-                        <a href="https://www.github.com" target='_blank' rel="noreferrer" >
-                            <img src={Github} style={{ height: "25px", background: 'white' }} alt='Github' />
+                        <a href="https://www.github.com" target="_blank" rel="noreferrer">
+                            <img src={Github} style={{ height: '25px', background: 'white' }} alt="Github" />
                         </a>
                     </div>
-                    <div class="social-media">
-
-                        <a href="https://www.instagram.com" target='_blank' rel="noreferrer" >
-                            <img src={Instagram} style={{ height: "25px", background: 'white' }} alt='Instagram' />
+                    <div className="social-media">
+                        <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
+                            <img src={Instagram} style={{ height: '25px', background: 'white' }} alt="Instagram" />
                         </a>
-                        {/* </div>
-                    <div class="social-media"> */}
-                        <a href="https://twitter.com" target='_blank' rel="noreferrer" >
-                            <img src={Github} style={{ height: "25px", background: 'white' }} alt='twitter' />
+                        <a href="https://twitter.com" target="_blank" rel="noreferrer">
+                            <img src={Github} style={{ height: '25px', background: 'white' }} alt="Twitter" />
                         </a>
                     </div>
-
-
                 </div>
-                <div>
-
-                </div>
-
+                <div></div>
             </div>
         </div>
-
-
-    )
-}
+    );
+};
 
 export default Footer1;
--- src/component/Header1.jsx
+++ src/component/Header1.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './header.css'; // Import your CSS files
 import './button1.css';
 import Footer1 from './Footer1';
@@ -6,15 +6,37 @@ import logo from './assets/logoCrazy.jpg';
 
 const Header1 = () => {
     const [navVisible, setNavVisible] = useState(false);
+    const [headingData, setHeadingData] = useState({
+        heading: 'Welcome to',
+        headingg: 'Crazy Web UI',
+    });
+
+    useEffect(() => {
+        // Fetch the heading data from the API
+        fetch('http://localhost:5000/hero/gethero')
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.success) {
+                    setHeadingData(data.data);
+                } else {
+                    // Handle error if needed
+                    console.error('Failed to fetch heading data');
+                }
+            })
+            .catch((error) => {
+                // Handle fetch error if needed
+                console.error('Error fetching heading data:', error);
+            });
+    }, []);
+
     const toggleNav = () => {
         setNavVisible(!navVisible);
     };
+
     return (
         <header className="boxy">
             <div className="div1">
-
                 <img className="imgz" src={logo} alt="Logo" />
-
                 <nav id="nav" className={`nav ${navVisible ? 'show-nav' : ''}`}>
                     <a href="{Footer1} target='_blank'">Home</a>
                     <a href="#">Services</a>
@@ -22,21 +44,21 @@ const Header1 = () => {
                     <a href="#">Skills</a>
                     <a href="#">Contact us</a>
                     <a href="#">My Blogs</a>
-
                 </nav>
             </div>
             <div id="polygon-image" className="heads">
                 <div className="flex-item">
-                    <h1>Welcome to</h1>
+                    <h1>{headingData.heading}</h1>
                 </div>
                 <div className="flex-item">
-                    <h1>CRAZY WEB UI</h1>
+                    <h1>{headingData.headingg}</h1>
                 </div>
                 <div className="flex-item">
                     <button className="button1">Let's be together</button>
                 </div>
             </div>
         </header>
-    )
-}
+    );
+};
+
 export default Header1;
